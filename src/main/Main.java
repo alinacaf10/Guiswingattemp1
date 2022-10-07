a6d9c5b1df655ccfa27b0e75134da435e6650ff8
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-07 16:23:02 +0400

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,41 @@
+package main;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton btn = new JButton();
+        JLabel label = new JLabel();
+        label.setText("0000");
+        label.setBounds(100, 50, 100, 30);
+        btn.setText("1-0");
+        btn.setBounds(100, 100, 100, 30);
+for (int x=0;x<10;x++){
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int i=0;
+                i++;
+                if (i%2==0){
+                    label.setText("0000");
+                }
+                else{
+                    label.setText("1111");
+                }
+            }
+        });
+    }
+
+        frame.setVisible(true);
+        frame.setSize(400, 400);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(btn);
+        frame.add(label);
+    }
+}
